ac9f04968ed3e6bf64620b98fd11a60fe8a332c7
Pavel Chuvak <chuvak.pavel@gmail.com>
2020-11-04 18:16:49 +0300

    Add Com.Igniscor.Progressbar package

---
+++ Com.Igniscor.Progressbar.Package/Com.Igniscor.Progressbar.Package.apple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Com.Igniscor.Progressbar.Package
+{
+    /// <summary>
+    /// Interface for Com.Igniscor.Progressbar.Package
+    /// </summary>
+    public class ProgressbarImplementation : IProgressbar
+    {
+    }
+}
---
+++ Com.Igniscor.Progressbar.Package/ICom.Igniscor.Progressbar.Package.shared.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Com.Igniscor.Progressbar.Package
+{
+    public interface IProgressbar
+    {
+    }
+}
